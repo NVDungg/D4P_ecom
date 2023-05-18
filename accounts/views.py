4fd55a157b383d037120dc0c16085065f282b72c
NVDungg <dung692000@gmail.com>
2023-05-18 21:53:20 +0700

    Add basic for accounts. Template register n login

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .forms import RegistrationForm
 
 # Create your views here.
+
+def register(request):
+    form =  RegistrationForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'accounts/register.html', context)
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    return 
