9abe0375a41483e56e5c4d322222a88cc3f950b7
NVDungg <dung692000@gmail.com>
2023-05-19 10:15:56 +0700

    Check match for password n comfirmpassw. Raise errors in template

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,7 +12,7 @@ class RegistrationForm(forms.ModelForm):
     }))
     confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': 'Enter Password Again',
-        'class':'form-control'
+        'class':'form-control' 
     }))
     class Meta:
         model = Account
@@ -33,3 +33,14 @@ class RegistrationForm(forms.ModelForm):
         #Loop through n add for all fields
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
+
+    #Check match password n confirm
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                " Password does match!"
+            )
