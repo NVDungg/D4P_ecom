214b6d5cd59e49b8c35c3342004424a3cb275497
NVDungg <dung692000@gmail.com>
2023-05-22 17:17:17 +0700

    fix bug in send email. Pass in app password

--- e_com/settings.py
+++ e_com/settings.py
@@ -158,8 +158,9 @@ MESSAGE_TAGS = {
 }
 
 #SMTP configuration
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'EcomVerification@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = config("HOST_USER")
+EMAIL_HOST_PASSWORD = config("HOST_PASSWORD")
 EMAIL_USE_TLS = True
