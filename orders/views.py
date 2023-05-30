a0e9ceed8570fde9a657733cc07e012c998daa59
NVDungg <dung692000@gmail.com>
2023-05-30 23:09:19 +0700

    creat forms for order

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from carts.models import CartItem
 
 # Create your views here.
+def place_order(request):
+    current_user = request.user
+    cart_items = CartItem.objects.filter(user=current_user)
+    cart_cout = cart_items.count()
+    if cart_cout <= 0:
+        return redirect('store')
+    
+    if request.method == POST:
+        form = OrderForm(request.POST)
