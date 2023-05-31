610fd00cad64cac7b9d65e1f8a7028eff7a33f02
NVDungg <dung692000@gmail.com>
2023-05-31 10:33:05 +0700

    Make order page paymments

--- orders/models.py
+++ orders/models.py
@@ -32,7 +32,7 @@ class Order(models.Model):
     address_line_1 = models.CharField(max_length=50)
     address_line_2 = models.CharField(max_length=50, blank=True)
     country = models.CharField(max_length=50)
-    state = models.CharField(max_length=50)
+    state = models.CharField(max_length=50, blank=True, null=True)
     city = models.CharField(max_length=50)
     order_note = models.CharField(max_length=100, blank=True)
     order_total = models.FloatField()
