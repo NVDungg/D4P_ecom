8708c8f15c668ad77131cf9b088802946716f473
NVDungg <dung692000@gmail.com>
2023-06-06 08:55:23 +0700

    generate invoice on order complete

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse, HttpResponse
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 
@@ -72,13 +73,12 @@ def payments(request):
     send_email = EmailMessage(mail_subject, message, to=[to_email])
     send_email.send()
 
-    '''# Send order number and transaction id back to sendData method via JsonResponse
+    # Send order number and transaction id back to sendData method via JsonResponse
     data = {
         'order_number': order.order_number,
         'transID': payment.payment_id,
     }
-    return JsonResponse(data)'''
-    return render(request, 'orders/payments.html')
+    return JsonResponse(data)
 
 def place_order(request, total = 0, quantity = 0):
     current_user = request.user
@@ -138,3 +138,29 @@ def place_order(request, total = 0, quantity = 0):
             return render(request, 'orders/payments.html', context)
         else:
             return redirect('checkout')
+
+def order_complete(request):
+    order_number = request.GET.get('order_number')
+    transID = request.GET.get('payment_id')
+
+    try:
+        order = Order.objects.get(order_number=order_number, is_ordered=True)
+        ordered_products = OrderProduct.objects.filter(order_id=order.id)
+
+        subtotal = 0
+        for i in ordered_products:
+            subtotal += i.product_price * i.quantity
+
+        payment = Payment.objects.get(payment_id=transID)
+
+        context = {
+            'order': order,
+            'ordered_products': ordered_products,
+            'order_number': order.order_number,
+            'transID': payment.payment_id,
+            'payment': payment,
+            'subtotal': subtotal,
+        }
+        return render(request, 'orders/order_complete.html', context)
+    except (Payment.DoesNotExist, Order.DoesNotExist):
+        return redirect('home')
