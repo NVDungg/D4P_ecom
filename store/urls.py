d4d292d65d814303595f3d4c944bccddc111ca55
NVDungg <dung692000@gmail.com>
2023-06-15 09:50:39 +0700

    Make rating n review function for product

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ProductListView, ProductDetailView, search
+from .views import ProductListView, ProductDetailView, search, submit_review
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="product_list"),
     path("category/<slug:category_slug>/", ProductListView.as_view(), name="product_category"),
     path("category/<slug:category_slug>/<slug:product_slug>/", ProductDetailView.as_view(), name="product_detail"),
     path("search/", search, name='search'),
+    path("submit_review/<int:product_id>/", submit_review, name='submit_review'),
 ]
